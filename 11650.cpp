07b3835c2342543acd1c45bed7d2062cdcde711f
Brad Lee <brad903@naver.com>
2018-05-10 17:10:31 +0900

    알고리즘 예제 및 문제풀이

---
+++ 11650.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+	vector<pair<int, int>> s;
+
+	for (int i = 0; i < n; i++) {
+		int a, b;
+		cin >> a >> b;
+		s.push_back(make_pair(a, b));
+	}
+
+	sort(s.begin(), s.end());  // pair를 정렬하면 먼저 first순서대로 정렬하고 이후 second 정렬
+
+	for (auto x : s) {
+		cout << x.first << ' ' << x.second << '\n';
+	}
+
+	return 0;
+}
