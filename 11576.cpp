3350ff14311d8ccfce7e50154f4c3f170109e9ed
Brad Lee <brad903@naver.com>
2018-05-22 13:37:21 +0900

    11576번 오류 코드 수정

--- 11576.cpp
+++ 11576.cpp
@@ -19,13 +19,18 @@ int main() {
 	for (int i = 0; i < m; i++) {
 		num = num * A + D[i];
 	}
+	
+	// 진법에 맞게 출력시키기 위해 벡터에 저장
+	vector<int> ans;
 
-	string ans;
-
-	while (num > 0) {
-		char remain = (num % B) + '0';
-		ans += remain;
-		num = num / B;
+	// 0일 경우 예외처리
+	if (num == 0) ans.push_back(0);
+	else {
+		while (num > 0) {
+			int remain = (num % B);
+			ans.push_back(remain);
+			num = num / B;
+		}
 	}
 
 	reverse(ans.begin(), ans.end());
