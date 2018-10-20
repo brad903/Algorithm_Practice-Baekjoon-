bf81c2092bd3aee9b6866f4ccc8afddf50870959
Brad Lee <brad903@naver.com>
2018-10-20 18:13:06 +0900

    baekjoon 5052 solved

---
+++ baekjoon-intellij/src/test/java/b5052/MainTest.java
@@ -0,0 +1,15 @@
+package b5052;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @Test
+    public void subStringTest() {
+        String test = "123456";
+        System.out.println(test.substring(0, 1));
+    }
+
+}
