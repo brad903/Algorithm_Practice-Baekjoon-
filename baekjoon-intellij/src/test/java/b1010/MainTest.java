afd928b586e7dc338ffb510c47a4eb9cd3a8a5a3
Brad Lee <brad903@naver.com>
2019-03-10 12:54:11 +0900

    b1010 해결중 - N을 그대로 계산해서는 안됨

---
+++ baekjoon-intellij/src/test/java/b1010/MainTest.java
@@ -0,0 +1,20 @@
+package b1010;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MainTest {
+    Main main = new Main();
+
+    @Test
+    public void factoryMethod() {
+        assertThat(Main.factoryMethod(4)).isEqualTo(24);
+    }
+
+    @Test
+    public void factoryMethod2() {
+        System.out.println(Main.factoryMethod(20));
+    }
+}
