5246bcd4c76cc06fe1467bb1375283a4fd6373bf
Brad Lee <brad903@naver.com>
2019-07-20 11:38:26 +0900

    10989 solved - changed print method using BufferedWriter

--- baekjoon-intellij/src/main/java/b10989/Main.java
+++ baekjoon-intellij/src/main/java/b10989/Main.java
@@ -1,27 +1,27 @@
 package b10989;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
-        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
             int n = Integer.parseInt(br.readLine());
-            Map<Integer, Integer> nums = new HashMap<>();
+            int[] nums = new int[10001];
+
             for (int i = 0; i < n; i++) {
                 int num = Integer.parseInt(br.readLine());
-                int value = nums.getOrDefault(num, 0) + 1;
-                nums.put(num, value);
+                nums[num]++;
             }
 
-            for (Integer key : nums.keySet()) {
-                for (int i = 0; i < nums.get(key); i++) {
-                    System.out.println(key);
+            try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+                for (int i = 0; i <= 10000; i++) {
+                    for (int j = 0; j < nums[i]; j++) {
+                        bw.write(i + "\n");
+                    }
                 }
+                bw.flush();
             }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
