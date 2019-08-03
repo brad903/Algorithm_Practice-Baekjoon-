fe067375500c095f9633e80f549c6502d3c27758
Brad Lee <brad903@naver.com>
2019-08-03 13:53:34 +0900

    7568 solved

--- baekjoon-nodejs/b7568/7568.js
+++ baekjoon-nodejs/b7568/7568.js
@@ -1,59 +1,20 @@
 var fs = require('fs');
 var input = fs.readFileSync('./stdin').toString().split('\n');
 
-var weights = new Array();
-var heights = new Array();
+var people = new Array();
 
-for(let i = 1; i <= input[0]; i++) {
-    let person = input[i].split(' ');
-    weights.push({weight: person[0], order: i - 1});
-    heights.push({height: person[1], order: i - 1});
+for(let index = 1; index <= input[0]; index++) {
+    var splitted = input[index].split(' ');
+    people.push({"weight": splitted[0], "height": splitted[1]});
 }
 
-heights.sort(function(a, b){
-    return b.height - a.height;
-}) 
-weights.sort(function(a, b){
-    return b.weight - a.weight;
-})
-
-var switches = new Array(parseInt(input[0]));
-for (let index = 0; index < switches.length; index++) {
-    switches[index] = false;
-}
-
-var ranks = new Array(parseInt(input[0]));
-var rank = 1;
-for (let index = 0; index < heights.length; index++) {
-    let heightOrder = heights[index].order;
-    let weightOrder = weights[index].order;
-
-    if(heightOrder != weightOrder) {
-        switches[heightOrder] = !switches[heightOrder]
-        switches[weightOrder] = !switches[weightOrder]
+for(let i = 0; i < people.length; i++) {
+    var rank = 1;
+    for(let j = 0; j < people.length; j++) {
+        if(i === j) continue;
+        if((people[i].weight < people[j].weight) && (people[i].height < people[j].height)) {
+            rank++;
+        }
     }
-
-    ranks[heightOrder] = rank;
-    ranks[weightOrder] = rank;
-
-    var pass = false;
-    for(let checkIndex = 0; checkIndex < heights.length; checkIndex++) {
-        if(switches[checkIndex]) pass = true;
-    }
-    if(pass || isSameWithNext(index)) continue;
-
-    if(rank !== index + 2) {
-        rank = index + 2;
-    }
-}
-
-var resultIndex = 0;
-for(resultIndex; resultIndex < ranks.length; resultIndex++) {
-    process.stdout.write(ranks[resultIndex] + " ");
-}
-
-function isSameWithNext(index) {
-    if(index === input[0] - 1) return false;
-    return (heights[index].height === heights[index + 1].height) &&
-     (weights[index].weight === weights[index + 1].weight)
-}
+    process.stdout.write(rank + " ");
+}   
