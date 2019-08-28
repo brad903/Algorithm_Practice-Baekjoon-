028f31a1253350f6fcce3738b335326c235d7109
Brad Lee <brad903@naver.com>
2019-08-29 00:23:16 +0900

    abundant file deleted:

--- baekjoon-java/baekjoon-cpp/11054-1.cpp
+++
@@ -1,5 +0,0 @@
-#include <iostream>
-#include <vector>
-#include <algoithm>
-using namespace std;
-
--- baekjoon-java/baekjoon-nodejs/b1085/b1085.js
+++
@@ -1,4 +0,0 @@
-var fs = require('fs');
-var input = fs.readFileSync('/dev/stdin').toString().split(' ');
-
-console.log(Math.min(input[0], input[1], input[2] - input[0], input[3] - input[1]));
