122f8533bd8f4c4a5a3432e384277046604b7cd2
Brad Lee <brad903@naver.com>
2019-09-06 02:18:44 +0900

    baekjoon 1157 solved

--- baekjoon-nodejs/b1157/index.js
+++ baekjoon-nodejs/b1157/index.js
@@ -1,21 +1,20 @@
 var fs = require('fs');
-var input = fs.readFileSync('./stdin').toString();
+var input = fs.readFileSync('./stdin').toString().toUpperCase();
+var arr = new Array(26).fill(0);
 
-var map = new Map();
-for(let i = 0; i < input.length; i++) {
-    let key = input[i].toUpperCase();
-    if(map.has(key)) map.set(key, map.get(key) + 1);
-    else map.set(key, 1);
+for (let i = 0; i < input.length; i++) {
+    arr[input.charCodeAt(i) - 65]++;
 }
 
-var arr = new Array();
-for (const key of map.keys()) {
-    arr.push([key, map.get(key)]);
+var result = '?'
+var count = 0;
+for (let i = 0; i < arr.length; i++) {
+    if(arr[i] > count) {
+        result = String.fromCharCode(i + 65);
+        count = arr[i];
+    }else if(arr[i] == count) {
+        result = '?';
+    }
 }
-arr.sort((a, b) => b[1] - a[1]);
 
-if(arr.length == 0) console.log("");
-if(arr.length == 1) console.log(arr[0][0]);
-
-if(arr[0][1] == arr[1][1]) console.log('?');
-else console.log(arr[0][0]);
+console.log(result);
